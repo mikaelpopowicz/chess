7762a173c8a0d41ba0ef180f438103f74154e019
Plancq Charlotte <plancq_c@epita.fr>
2015-05-26 17:24:44 +0200

    First definition of GameEngine

---
+++ src/game-engine.hh
@@ -0,0 +1,46 @@
+#ifndef GAME_ENGINE_HH
+# define GAME_ENGINE_HH
+
+#include <vector>
+#include "player.hh"
+#include "observer.hh"
+#include "chessboard.hh"
+
+/*!
+** \brief The game engine, which check moves, rules and play the game
+*/
+class GameEngine
+{
+public:
+  GameEngine(Player p1, Player p2);
+  virtual ~GameEngine();
+
+  void set_observer(Observer o);
+  /*!
+  ** Play the game.
+  ** @return 2 if a move is invalid, else 0
+  */
+  int play();
+
+protected:
+  /*!
+  ** Represents the number of moves since:
+  **   - no piece has been taken &
+  **   - no pawn has been moved
+  */
+  int nb_turn_no_move_;
+  /*!
+  ** The current chessboard
+  */
+  Chessboard actual_;
+  /*!
+  ** This history is used to check for the threefold repetition
+  ** (if the exact same configuration occurs three times).
+  */
+  std::vector<Chessboard> history_;
+
+  bool check_move();
+  bool check_rules();
+  void add_history(Chessboard cb);
+  void clear_history();
+}
