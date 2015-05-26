e1fb69c9ed05f03a957004f66c153e21158e3c1e
POPOWICZ Mikaël <popowi_m@epita.fr>
2015-05-26 16:21:51 +0200

    First definition of option parser

--- src/option-parser.hh
+++ src/option-parser.hh
@@ -1,3 +1,33 @@
-OptionParser {
+#ifndef OPTION_PARSER_HH_
+# define OPTION_PARSER_HH_
 
+# include <string>
+
+/*!
+** \brief Options parser
+*/
+class OptionParser
+{
+public:
+  OptionParser(int argc, char *argv[]);
+  ~OptionParser();
+  
+  /*!
+  ** Parse the command line options
+  ** @return bool If errors occured
+  */
+  bool parse();
+  
+  bool isPgn();
+  std::string getFile();
+  
+private:
+  int argc_;
+  char *argv_[];
+  bool pgn_;
+  std::string file_;
+  std::string player1_;
+  std::string player2_;
 };
+
+#endif /* !OPTION_PARSER_HH_ */
