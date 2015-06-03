4f42f4325caceb9ad6df58a0547f0c859dadbdc5
Plancq Charlotte <plancq_c@epita.fr>
2015-06-03 14:28:47 +0200

    DEBUG castling move in PlayerPGN

--- src/player/player-pgn.cc
+++ src/player/player-pgn.cc
@@ -170,18 +170,20 @@ void PlayerPGN::last_opponent_move_set(const Move& last_opponent_move)
 Move PlayerPGN::little_rock()
 {
   std::cout << "LITTLE ROCK METHOD" << std::endl;
+  Position begin(Position::FILE_FIRST, Position::RANK_FIRST);
+  Position end(Position::FILE_FIRST, Position::RANK_FIRST);
   if (color_ == Color::WHITE)
   {
-    Position begin(Position::EVA, Position::EINS);
-    Position end(Position::GUSTAV, Position::EINS);
-    return Move(begin, end, PieceType::NONE);
+    begin = Position(Position::EVA, Position::EINS);
+    end = Position(Position::GUSTAV, Position::EINS);
   }
   else
   {
-    Position begin(Position::EVA, Position::ACHT);
-    Position end(Position::GUSTAV, Position::ACHT);
-    return Move(begin, end, PieceType::NONE);
+    begin = Position(Position::EVA, Position::ACHT);
+    end = Position(Position::GUSTAV, Position::ACHT);
   }
+  board_.make_move(Move(begin, end));
+  return Move(begin, end, PieceType::NONE);
 }
 
 Move PlayerPGN::big_rock()
