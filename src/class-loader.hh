0d1e3937caa67a90f72f733e6e781fb41d2ff839
POPOWICZ Mikaël <popowi_m@epita.fr>
2015-06-08 21:25:47 +0200

    Add ClassLoader::get_ia(std::string, Color)

--- src/class-loader.hh
+++ src/class-loader.hh
@@ -6,6 +6,8 @@
 # include <string>
 # include <dlfcn.h>
 # include "listener.hh"
+# include "ai.hh"
+# include "color.hh"
 
 class ClassLoader
 {
@@ -14,6 +16,7 @@ public:
   ~ClassLoader();
 
   bool load_libraries(std::vector<std::string> libs);
+  Ai* get_ia(std::string ia, Color color);
   std::vector<void*> get_libs();
   std::vector<ListenerExport*> get_plugins();
 
